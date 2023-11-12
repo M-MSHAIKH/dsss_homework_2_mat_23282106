c06c8212b7b109839c80accbfce3cc114c88e340
M-MSHAIKH <150423227+M-MSHAIKH@users.noreply.github.com>
2023-11-13 03:25:02 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dsss_homework_2_mat_23282106',
+    version='3.11.3',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
