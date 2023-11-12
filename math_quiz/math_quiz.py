12c19dba199c96ebee464d454377fc55176a2c73
M-MSHAIKH <150423227+M-MSHAIKH@users.noreply.github.com>
2023-11-12 15:47:06 +0530

    Update math_quiz.py

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,21 +1,25 @@
+#Importing random numbers and performing mathematical tasks on it
 import random
 
-
-def function_A(min, max):
+# defining function for generating random number (integer) between the min and max of the random number
+#Also, return statements ensures that generated random number will be used for further coding
+def min_max(min, max):
     """
     Random integer.
     """
     return random.randint(min, max)
 
-
-def function_B():
+# defining function for the choice of an arithmetical operations
+#return statements ensure that generated choice of operation stored and will be used in the upcoming code
+def arithmatic_operations():
     return random.choice(['+', '-', '*'])
 
-
-def function_C(n1, n2, o):
+# defining a third function for performing an arithmetic operations using value of n1 n2 and o
+# saves the value of p and a by returing the values of the same
+def claculations(n1, n2, o):
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
+    if o == '+': a = n1 + n2   # Changing the sign for performing correct operations from - to +
+    elif o == '-': a = n1 + n2  # Changing the sign for performing correct operations from - to +
     else: a = n1 * n2
     return p, a
 
@@ -27,9 +31,9 @@ def math_quiz():
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        n1 =  min_max(1, 10); n2 = min_max(1, 5.5); o = arithmatic_operations()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = claculations(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
