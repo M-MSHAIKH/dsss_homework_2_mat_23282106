9f18f860fee04babe944f46374997201d8a1584e
M-MSHAIKH <150423227+M-MSHAIKH@users.noreply.github.com>
2023-11-13 11:15:27 +0530

    Update tests_math_quiz.py

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,47 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import min_max, arithmatic_operations, calculations
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
-        # Test if random numbers generated are within the specified range
+    def test_min_max(self):
+        # Test if the random numbers generated is within the specified range, set by the user
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = min_max(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def tes_arithmatic_operations(self):
+        # This function test whether generated sign is valid
+        valid_signs = ['+', '-', '*']
+        for _ in range(len(valid_signs)):
+            result = choose_sign()
+            self.assertIn(result, valid_signs, f"The result '{result}' is not a valid mathematical sign")
+            pass
 
-    def test_function_C(self):
+
+
+    def test_calculations(self):
+        # This funcation check validity of the Arithmatic Operation
+
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (8, 5, '*', '8 * 5', 40),
+                (2, 1, '-', '5 - 7', 1),
+                (8, 8, '-', '8 - 8', 0),
+                (6, 5, '-', '6 + 5', 11)
             ]
 
+
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
+                PROBLEM, ANSWER = apply_operation(num1,num2,operator)
+                self.assertEqual(expected_problem,PROBLEM, f"The expected problem don't match the problem. Expected Problem: {expected_problem}, Problem: {PROBLEM}")
+                self.assertEqual(expected_answer,ANSWER, f"The expected problem don't match the problem. Expected Answer: {expected_answer}, Answer: {ANSWER}")
                 pass
+
+
 
 if __name__ == "__main__":
     unittest.main()
+
